d491f76fd5549caf699c2f146af0e29b6ff3f5f0
Manmit <manmitsamal84@gmail.com>
2025-03-22 20:21:18 +0530

    Update Main.java

--- MSE-2/Inheritance/SocialMediaPlatform/src/Main.java
+++ MSE-2/Inheritance/SocialMediaPlatform/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         RegularUser ru = new RegularUser("hello_john", "john.4@gayhoo.com", 5);
         ru.displayDetails();
-
+        System.out.println("---------------------------");
         AdminUser au = new AdminUser("hello_kelly", "kelly.69@lesbohoo.com", 69);
         au.displayDetails();
     }
